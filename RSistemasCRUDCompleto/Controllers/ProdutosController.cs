08997c2164f3b03e71156bcc0c7b7f55af679192
Lucas Daniel <lucasfelepe@gmail.com>
2021-01-14 19:09:45 -0300

    Adicionar arquivos de projeto.

---
+++ RSistemasCRUDCompleto/Controllers/ProdutosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSistemasCRUDCompleto.Controllers
+{
+    public class ProdutosController : Controller
+    { 
+        // GET: /Produtos/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: /Produtos/Privacy
+        public IActionResult Privacy() {
+            return View();
+        }
+        // GET: /Produtos/Welcome
+        public IActionResult Welcome(String name="Lucas", int numTimes = 1)
+        {
+            ViewData["Mensagem"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
